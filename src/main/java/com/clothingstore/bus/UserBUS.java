dbbbf128b0f15f6080d0e2c9ea91df964cc92e3d
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-10-06 10:45:28 +0700

    code userbus

--- src/main/java/com/clothingstore/bus/UserBUS.java
+++ src/main/java/com/clothingstore/bus/UserBUS.java
@@ -1,52 +1,210 @@
 package com.clothingstore.bus;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import com.clothingstore.dao.UserDAO;
+import com.clothingstore.enums.UserStatus;
 import com.clothingstore.interfaces.IBUS;
 import com.clothingstore.models.UserModel;
+import services.Validation;
 
 public class UserBUS implements IBUS<UserModel> {
 
+    private final List<UserModel> userList = new ArrayList<>();
+    private static UserBUS instance;
+
+    public static UserBUS getInstance() {
+        if (instance == null) {
+            instance = new UserBUS();
+        }
+        return instance;
+    }
+
+    private UserBUS() {
+        this.userList.addAll(UserDAO.getInstance().readDatabase());
+    }
+
     @Override
     public List<UserModel> getAllModels() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllModels'");
+        return Collections.unmodifiableList(userList);
     }
 
     @Override
     public void refreshData() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'refreshData'");
+        userList.clear();
+        userList.addAll(UserDAO.getInstance().readDatabase());
     }
 
     @Override
     public UserModel getModelById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getModelById'");
+        for (UserModel userModel : userList) {
+            if (userModel.getId() == id) {
+                return userModel;
+            }
+        }
+
+        UserModel userModel = UserDAO.getInstance().getUserById(id);
+        if (userModel != null) {
+            userList.add(userModel);
+        }
+        return userModel;
     }
 
     @Override
-    public int addModel(UserModel model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addModel'");
+    public int addModel(UserModel userModel) {
+        if (userModel.getUsername() == null ||
+                userModel.getUsername().isEmpty() ||
+                userModel.getName() == null ||
+                userModel.getName().isEmpty() ||
+                userModel.getPassword() == null ||
+                userModel.getPassword().isEmpty()) {
+            throw new IllegalArgumentException(
+                    "Username, name and password cannot be empty. Please check the input and try again.");
+        }
+
+        boolean hasPhone = userModel.getPhone() != null && !userModel.getPhone().isEmpty();
+        boolean hasEmail = userModel.getEmail() != null && !userModel.getEmail().isEmpty();
+
+        if (hasEmail && !Validation.isValidEmail(userModel.getEmail())) {
+            throw new IllegalArgumentException("Invalid email address.");
+        }
+
+        if (hasPhone && !Validation.isValidPhoneNumber(userModel.getPhone())) {
+            throw new IllegalArgumentException("Invalid number format.");
+        }
+        // 0 is admin, 1 is manager, 2 is employee
+        userModel.setRoleId(2);
+
+        userModel.setUserStatus(
+                userModel.getUserStatus() != null ? userModel.getUserStatus() : UserStatus.ACTIVE);
+        int id = UserDAO.getInstance().insert(mapToEntity(userModel));
+        userModel.setId(id);
+        userList.add(userModel);
+        return id;
     }
 
     @Override
-    public int updateModel(UserModel model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateModel'");
+    public int updateModel(UserModel userModel) {
+        int updatedRows = UserDAO.getInstance().update(userModel);
+        if (updatedRows > 0) {
+            for (int i = 0; i < userList.size(); i++) {
+                if (userList.get(i).getId() == userModel.getId()) {
+                    userList.set(i, userModel);
+                    break;
+                }
+            }
+        }
+        return updatedRows;
     }
 
     @Override
     public int deleteModel(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteModel'");
+        UserModel userModel = getModelById(id);
+        if (userModel == null) {
+            throw new IllegalArgumentException(
+                    "User with ID " + id + " does not exist.");
+        }
+        int deletedRows = UserDAO.getInstance().delete(id);
+        if (deletedRows > 0) {
+            userList.remove(userModel);
+        }
+        return deletedRows;
     }
 
     @Override
     public List<UserModel> searchModel(String value, String[] columns) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'searchModel'");
+        value.toLowerCase();
+        List<UserModel> results = new ArrayList<>();
+        List<UserModel> entities = UserDAO.getInstance().search(value, columns);
+        for (UserModel entity : entities) {
+            UserModel model = mapToEntity(entity);
+            if (checkFilter(model, value, columns)) {
+                results.add(model);
+            }
+        }
+
+        return results;
+    }
+
+    private UserModel mapToEntity(UserModel from) {
+        UserModel to = new UserModel();
+        updateEntityFields(from, to);
+        return to;
+    }
+
+    private void updateEntityFields(UserModel from, UserModel to) {
+        to.setId(from.getId());
+        to.setUsername(from.getUsername());
+        to.setPassword(from.getPassword());
+        to.setUserStatus(from.getUserStatus());
+        to.setName(from.getName());
+        to.setEmail(from.getEmail());
+        to.setPhone(from.getPhone());
+        to.setRoleId(from.getRoleId());
+    }
+
+    private boolean checkFilter(
+            UserModel userModel,
+            String value,
+            String[] columns) {
+        value.toLowerCase();
+        for (String column : columns) {
+            switch (column.toLowerCase()) {
+                case "id" -> {
+                    if (userModel.getId() == Integer.parseInt(value)) {
+                        return true;
+                    }
+                }
+                case "username" -> {
+                    if (userModel.getUsername().toLowerCase().contains(value)) {
+                        return true;
+                    }
+                }
+                case "status" -> {
+                    if (userModel.getUserStatus().toString().equals(value)) {
+                        return true;
+                    }
+                }
+                case "name" -> {
+                    if (userModel.getName().contains(value)) {
+                        return true;
+                    }
+                }
+                case "email" -> {
+                    if (userModel.getEmail().contains(value)) {
+                        return true;
+                    }
+                }
+                case "phone" -> {
+                    if (userModel.getPhone().equals(value)) {
+                        return true;
+                    }
+                }
+                case "role" -> {
+                    if (String.valueOf(userModel.getRoleId()).contains(value)) {
+                        return true;
+                    }
+                }
+                default -> {
+                    if (checkAllColumns(userModel, value)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean checkAllColumns(UserModel userModel, String value) {
+        return (userModel.getId() == Integer.parseInt(value) ||
+                userModel.getUsername().equalsIgnoreCase(value) ||
+                userModel.getUserStatus().toString().equalsIgnoreCase(value) ||
+                userModel.getName().equalsIgnoreCase(value) ||
+                userModel.getEmail().equalsIgnoreCase(value) ||
+                userModel.getPhone().equals(value) ||
+                String.valueOf(userModel.getRoleId()).equalsIgnoreCase(value));
     }
 
 }
