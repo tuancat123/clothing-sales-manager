fce3b382a8b939958010f6ea733a9565c01caf92
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2023-10-27 15:14:59 +0700

    GUI LOGIN

--- src/main/java/com/clothingstore/gui/login/Login.java
+++ src/main/java/com/clothingstore/gui/login/Login.java
@@ -1,27 +1,27 @@
 package com.clothingstore.gui.login;
 
-// import com.bookstore.bus.UserBUS;
-// import com.bookstore.gui.components.buttons.Button;
-// import com.bookstore.gui.factories.UIFactory;
-// import com.bookstore.models.UserModel;
-// import com.bookstore.services.Authentication;
-// import com.bookstore.util.InputValidator;
+import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.LayoutManager;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
+import javax.swing.border.Border;
+
+import org.apache.poi.ss.usermodel.Color;
 
 import com.clothingstore.bus.UserBUS;
 import com.clothingstore.models.UserModel;
@@ -32,18 +32,15 @@ public class Login extends JFrame {
 
     private static Login instance;
 
-    private LayoutManager layout;
     private JPanel loginPanel;
-    private JButton exitButton;
     private JButton loginButton;
     private JLabel LogoLabel;
     private JLabel passwordLabel;
     private JLabel usernameLabel;
-    private JPasswordField passwordField;
-    private JTextField usernameTextField;
-    private JToggleButton toggleButton;
-    private ImageIcon showPassword;
-    private ImageIcon hidePassword;
+    private JPasswordField txtPassWord;
+    private JLabel eyeLabel;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
 
     public static Login getInstance() {
         if (instance == null) {
@@ -55,103 +52,140 @@ public class Login extends JFrame {
     private Login() {
         initComponents();
         getRootPane().setDefaultButton(loginButton);
-        setPreferredSize(new Dimension(700, 600));
-        setMinimumSize(new Dimension(700, 600));
+        setPreferredSize(new Dimension(1000, 550));
+        setMinimumSize(new Dimension(1000, 550));
+        setBackground(java.awt.Color.WHITE);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
     }
 
     private void initComponents() {
-        layout = new FlowLayout(FlowLayout.CENTER, 10, 10);
-        setLayout(layout);
+        setLayout(new BorderLayout());
         LogoLabel = new JLabel();
-        LogoLabel.setIcon(new ImageIcon("src/main/java/resources/images/logo.png"));
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        JLabel LogoLabel = new JLabel(new ImageIcon("src/main/java/resources/images/logo1.png"));
+        leftPanel.add(LogoLabel, BorderLayout.CENTER);
+        leftPanel.setBackground(java.awt.Color.WHITE);
+        add(leftPanel, BorderLayout.WEST);
 
         loginPanel = new JPanel();
+        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.PAGE_AXIS));
+        loginPanel.setBackground(new java.awt.Color(173, 216, 230));
 
+        jLabel3 = new JLabel();
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 36));
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jLabel3.setText("Login");
+        loginPanel.add(jLabel3);
+
+        jLabel4 = new JLabel();
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jLabel4.setText("Hello! Let's get started");
+        loginPanel.add(jLabel4);
+
+        loginPanel.add(Box.createVerticalGlue());
+        loginPanel.add(jLabel3);
+        loginPanel.add(jLabel4);
+        loginPanel.add(Box.createVerticalGlue());
+
+        JPanel grUserName = new JPanel();
+        grUserName.setLayout(new FlowLayout());
+        grUserName.setLayout(new FlowLayout(FlowLayout.CENTER, 15, 0));
+        grUserName.setBackground(new java.awt.Color(173, 216, 230));
+        grUserName.setPreferredSize(new Dimension(200, 0));
         usernameLabel = new JLabel();
+        usernameLabel.setFont(new java.awt.Font("Segoe UI", 0, 13));
+        usernameLabel.setForeground(java.awt.Color.BLACK);
         usernameLabel.setText("Username");
+        JTextField txtusername = new JTextField();
+        txtusername.setColumns(20);
+        txtusername.setPreferredSize(new Dimension(200, 30));
+        txtusername.setBorder(null);
+        JLabel userLabel = new JLabel(new ImageIcon("src/main/java/resources/icons/icons8_user_20px_1.png"));
+        userLabel.setBackground(java.awt.Color.BLACK);
+        grUserName.add(usernameLabel);
+        grUserName.add(txtusername);
+        grUserName.add(userLabel);
+
+        JPanel grPassWord = new JPanel();
+        grPassWord.setLayout(new FlowLayout());
+        grPassWord.setLayout(new FlowLayout(FlowLayout.CENTER, 15, 0));
+        grPassWord.setPreferredSize(new Dimension(200, 0));
+        grPassWord.setBackground(new java.awt.Color(173, 216, 230));
         passwordLabel = new JLabel();
         passwordLabel.setText("Password");
-        usernameTextField = new JTextField();
-        passwordField = new JPasswordField();
-        loginButton = new JButton("Login");
-        exitButton = new JButton("Exit");
-        showPassword = new ImageIcon("src/main/java/resources/icons/show_password.png");
-        hidePassword = new ImageIcon("src/main/java/resources/icons/hide_password.png");
-        toggleButton = new JToggleButton(hidePassword);
-        loginButton.addActionListener(loginButtonActionListener);
-        exitButton.addActionListener(exitButtonActionListener);
-        toggleButton.addActionListener(showPasswordButtonActionListener);
+        passwordLabel.setFont(new java.awt.Font("Segoe UI", 0, 13));
+        passwordLabel.setForeground(java.awt.Color.BLACK);
+        passwordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        grPassWord.add(passwordLabel);
+        txtPassWord = new JPasswordField();
+        txtPassWord.setColumns(20);
+        txtPassWord.setPreferredSize(new Dimension(200, 30));
+        txtPassWord.setEchoChar('\u25cf');
+        txtPassWord.setBorder(null);
+        eyeLabel = new JLabel(new ImageIcon("src/main/java/resources/icons/icons8_invisible_20px_1.png"));
+        eyeLabel.setBackground(java.awt.Color.BLACK);
+        eyeLabel.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                showPasswordButtonActionListener();
+            }
+        }); // sự kiện khi ấn nút mắt
 
-        loginPanel.setLayout(new GridBagLayout());
-        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        loginButton = new JButton("LOGIN");
+        loginButton.setBackground(new java.awt.Color(255, 255, 255));
+        loginButton.setFont(new java.awt.Font("Segoe UI", 0, 16));
+        loginButton.setForeground(new java.awt.Color(0, 0, 128));
+        loginButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginButton.setMargin(new Insets(6, 120, 6, 120));
+        grPassWord.add(txtPassWord);
+        grPassWord.add(eyeLabel);
 
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        loginPanel.add(grUserName);
+        loginPanel.add(grPassWord);
+        loginPanel.add(loginButton);
+        loginPanel.add(Box.createVerticalGlue());
 
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 4;
-        loginPanel.add(usernameLabel, gridBagConstraints);
-
-        gridBagConstraints.gridy = 1;
-        loginPanel.add(usernameTextField, gridBagConstraints);
-
-        gridBagConstraints.gridy = 2;
-        loginPanel.add(passwordLabel, gridBagConstraints);
-        gridBagConstraints.gridy = 3;
-        loginPanel.add(passwordField, gridBagConstraints);
-        JButton addButton = new JButton("");
-
-        addButton.add(toggleButton);
-
-        gridBagConstraints.gridx = 4;
-        gridBagConstraints.gridy = 3;
-
-        loginPanel.add(toggleButton, gridBagConstraints);
-
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridwidth = 1;
-        gridBagConstraints.gridy = 4;
-        loginPanel.add(loginButton, gridBagConstraints);
-        gridBagConstraints.gridx = 3;
-        loginPanel.add(exitButton, gridBagConstraints);
-
-        add(LogoLabel);
-        add(loginPanel);
+        add(loginPanel, BorderLayout.CENTER);
     }
 
-    private ActionListener showPasswordButtonActionListener = e -> {
-        if (toggleButton.isSelected()) {
-            passwordField.setEchoChar((char) 0);
-            toggleButton.setIcon(showPassword);
-        } else {
-            passwordField.setEchoChar('\u25cf');
-            toggleButton.setIcon(hidePassword);
-        }
-    };
+    private void showPasswordButtonActionListener() {
+        ImageIcon eyeOpenIcon = new ImageIcon("src/main/java/resources/icons/icons8_eye_20px_1.png");
+        ImageIcon eyeCloseIcon = new ImageIcon("src/main/java/resources/icons/icons8_invisible_20px_1.png");
 
-    private ActionListener loginButtonActionListener = e -> {
+        // Đổi trạng thái hiển thị mật khẩu
+        if (txtPassWord != null) {
+            if (txtPassWord.getEchoChar() == 0) {
+                // Hiển thị mật khẩu
+                txtPassWord.setEchoChar('\u25cf');
+                eyeLabel.setIcon(eyeCloseIcon);
+            } else {
+                // Ẩn mật khẩu
+                txtPassWord.setEchoChar('\u0000');
+                eyeLabel.setIcon(eyeOpenIcon);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         try {
-            String username = usernameTextField.getText();
-            String password = String.valueOf(passwordField.getPassword());
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    Login login = new Login();
+                    login.setVisible(true);
 
-            UserModel user = UserBUS.getInstance().login(username, password);
-            Authentication.setCurrentUser(user);
-            dispose();
-        } catch (Exception exception) {
-            JOptionPane.showMessageDialog(
-                    null,
-                    exception.getMessage(),
-                    "Error",
-                    JOptionPane.ERROR_MESSAGE);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        passwordField.setText("");
-    };
-
-    private ActionListener exitButtonActionListener = e -> {
-        System.exit(0);
-    };
+    }
 
 }
