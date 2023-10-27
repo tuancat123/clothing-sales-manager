f0a14c67f994281fc42b0323db44d870ebecf45d
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-10-27 10:45:05 +0700

    fix database insert and init

--- database/init/init.sql
+++ database/init/init.sql
@@ -81,7 +81,7 @@ CREATE TABLE
         `customer_id` INT,
         `user_id` INT,
         `order_date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        `total_amount` DOUBLE NOT NULL,
+        `total_price` DOUBLE NOT NULL,
         PRIMARY KEY (`id`)
     );
 
