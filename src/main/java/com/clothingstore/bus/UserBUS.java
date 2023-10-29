bf3412787e0e91e58ef7b0a3f4ea8feab28a7a61
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-10-29 11:18:19 +0700

    fix logic add model

--- src/main/java/com/clothingstore/bus/UserBUS.java
+++ src/main/java/com/clothingstore/bus/UserBUS.java
@@ -70,6 +70,19 @@ public class UserBUS implements IBUS<UserModel> {
           "Username, name and password cannot be empty. Please check the input and try again.");
     }
 
+    ArrayList<UserModel> userList = UserDAO.getInstance().readDatabase();
+    for (UserModel userModel2 : userList) {
+      if (userModel2.getUsername().equals(userModel.getUsername())) {
+        throw new IllegalArgumentException("Username is exist");
+      }
+      if (userModel2.getEmail().equals(userModel.getEmail())) {
+        throw new IllegalArgumentException("Email is exist");
+      }
+      if (userModel2.getPhone().equals(userModel.getPhone())) {
+        throw new IllegalArgumentException("Phone is exist");
+      }
+    }
+
     boolean hasPhone = userModel.getPhone() != null && !userModel.getPhone().isEmpty();
     boolean hasEmail = userModel.getEmail() != null && !userModel.getEmail().isEmpty();
 
