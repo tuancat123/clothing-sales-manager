8bd8190e61a68223daf8acc8c03e6f8d43c54ef0
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-10-30 17:35:57 +0700

    clean code

--- src/main/java/com/clothingstore/Application.java
+++ src/main/java/com/clothingstore/Application.java
@@ -2,10 +2,7 @@ package com.clothingstore;
     
 import javax.swing.*;
 
-import com.clothingstore.gui.admin.dashboard.Dashboard;
-import com.clothingstore.gui.employee.HomePage;
 import com.clothingstore.gui.login.Login;
-import com.clothingstore.gui.manager.Main;
 
 public class Application {
     public static void main(String[] args) {
