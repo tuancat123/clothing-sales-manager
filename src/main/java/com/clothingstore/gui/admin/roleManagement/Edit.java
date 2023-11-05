0c61fa18300bbd5986d9936600c01a2cbb1ee182
qcuong57 <hoquoccuong945@gmail.com>
2023-11-05 19:59:10 +0700

    role list

---
+++ src/main/java/com/clothingstore/gui/admin/roleManagement/Edit.java
@@ -0,0 +1,143 @@
+package com.clothingstore.gui.admin.roleManagement;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+
+public class Edit extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField textField_id;
+
+
+    public Edit(){
+        initComponents();
+    }
+
+    public void initComponents() {
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 647, 577);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        this.setLocationRelativeTo(null);
+        setResizable(false);
+
+        setContentPane(contentPane);
+        contentPane.setLayout(new BorderLayout(0, 0));
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500,50));
+        panel.setBackground(new Color(0, 38, 77));
+        contentPane.add(panel, BorderLayout.NORTH);
+
+        JLabel lblNewLabel = new JLabel("Edit Role Permission");
+        lblNewLabel.setForeground(new Color(255, 255, 255));
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+        panel.add(lblNewLabel);
+
+        JPanel panel_1 = new JPanel();
+        contentPane.add(panel_1, BorderLayout.CENTER);
+        panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+
+        JPanel panel_3 = new JPanel();
+        panel_3.setBorder(new TitledBorder(null, "Role information",
+                TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        panel_1.add(panel_3);
+        GridBagLayout gbl_panel_3 = new GridBagLayout();
+        gbl_panel_3.columnWidths = new int[]{78, 180, 0, 75, 166, 0, 0, 0};
+        gbl_panel_3.rowHeights = new int[]{0, 43, 37, 77, 36, 0, 0};
+        gbl_panel_3.columnWeights = new double[]{0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+        gbl_panel_3.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+        panel_3.setLayout(gbl_panel_3);
+
+        JLabel lbl_id = new JLabel("ID");
+        GridBagConstraints gbc_lbl_id = new GridBagConstraints();
+        gbc_lbl_id.insets = new Insets(0, 0, 5, 5);
+        gbc_lbl_id.gridx = 0;
+        gbc_lbl_id.gridy = 1;
+        panel_3.add(lbl_id, gbc_lbl_id);
+
+        textField_id = new JTextField();
+        textField_id.setEditable(false);
+        GridBagConstraints gbc_textField_id = new GridBagConstraints();
+        gbc_textField_id.insets = new Insets(0, 0, 5, 5);
+        gbc_textField_id.fill = GridBagConstraints.BOTH;
+        gbc_textField_id.gridx = 1;
+        gbc_textField_id.gridy = 1;
+        panel_3.add(textField_id, gbc_textField_id);
+        textField_id.setColumns(10);
+
+        JLabel lbl_permission = new JLabel("Role");
+        GridBagConstraints gbc_lbl_permission = new GridBagConstraints();
+        gbc_lbl_permission.insets = new Insets(0, 0, 5, 5);
+        gbc_lbl_permission.gridx = 0;
+        gbc_lbl_permission.gridy = 2;
+        panel_3.add(lbl_permission, gbc_lbl_permission);
+        
+        JComboBox comboBox_role = new JComboBox();
+        GridBagConstraints gbc_comboBox_role = new GridBagConstraints();
+        gbc_comboBox_role.insets = new Insets(0, 0, 5, 5);
+        gbc_comboBox_role.fill = GridBagConstraints.BOTH;
+        gbc_comboBox_role.gridx = 1;
+        gbc_comboBox_role.gridy = 2;
+        panel_3.add(comboBox_role, gbc_comboBox_role);
+
+        JLabel lbl_password = new JLabel("Permission");
+        GridBagConstraints gbc_lbl_password = new GridBagConstraints();
+        gbc_lbl_password.insets = new Insets(0, 0, 5, 5);
+        gbc_lbl_password.gridx = 0;
+        gbc_lbl_password.gridy = 3;
+        panel_3.add(lbl_password, gbc_lbl_password);
+        
+        JTextArea textArea_permission = new JTextArea();
+        GridBagConstraints gbc_textArea_permission = new GridBagConstraints();
+        gbc_textArea_permission.insets = new Insets(0, 0, 5, 5);
+        gbc_textArea_permission.fill = GridBagConstraints.BOTH;
+        gbc_textArea_permission.gridx = 1;
+        gbc_textArea_permission.gridy = 3;
+        panel_3.add(textArea_permission, gbc_textArea_permission);
+
+        JPanel panel_Model = new JPanel();
+        panel_Model.setPreferredSize(new Dimension(500,100));
+        panel_Model.setBorder(new TitledBorder(null, "Button List",
+                TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        contentPane.add(panel_Model, BorderLayout.SOUTH);
+
+        JButton btnEdit = new JButton("Edit");
+        btnEdit.setPreferredSize(new Dimension(100,30));
+        btnEdit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        panel_Model.add(btnEdit);
+
+        JButton btnReset = new JButton("Reset");
+        btnReset.setPreferredSize(new Dimension(100,30));
+        btnReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        panel_Model.add(btnReset);
+
+        JButton btnCancel = new JButton("Cancel");
+        btnCancel.setPreferredSize(new Dimension(100,30));
+        panel_Model.add(btnCancel);
+        btnCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Window window = SwingUtilities.getWindowAncestor((Component)e.getSource());
+                if (window != null) {
+                    window.dispose();
+                }
+            }
+        });
+
+        JPanel panel_4 = new JPanel();
+        panel_Model.add(panel_4);
+        panel_4.setLayout(new GridLayout(1, 0, 0, 0));
+    }
+
+}
---
+++ src/main/java/com/clothingstore/gui/admin/roleManagement/RoleManagement.java
@@ -0,0 +1,199 @@
+package com.clothingstore.gui.admin.roleManagement;
+
+
+
+import com.clothingstore.gui.components.Menu;
+import com.clothingstore.gui.models.MenuData;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+
+public class RoleManagement extends JFrame {
+    private static RoleManagement instance;
+    private JTextField textField;
+    private JPanel employeePanel;
+    private JPanel topPanel;
+    private JLabel lbl_title;
+    private JLabel titleLabel;
+    private JPanel panel;
+    private JPanel centerPanel;
+    private JLabel lblNewLabel_1;
+    private JPanel panel_Search;
+    private JPanel panel_Search_1;
+    private JPanel bottomPanel;
+    private JPanel panel_table;
+    private JPanel panel_Model;
+    private JButton btnAdd;
+    private JButton btnDelete;
+    private JButton btnEdit;
+    private JButton btnSearch;
+    private JPanel panel_Role;
+
+    public static RoleManagement getInstance() {
+        if (instance == null) {
+            instance = new RoleManagement();
+        }
+        return instance;
+    }
+
+    public RoleManagement(){
+        initComponents();
+    }
+
+    public void initComponents() {
+
+        employeePanel = new JPanel();
+
+        getContentPane().setLayout(new BorderLayout());
+        setSize(new Dimension(1130, 628));
+        employeePanel.setBackground(new Color(0, 38, 77));
+        employeePanel.setBorder(BorderFactory.createEmptyBorder(1, 20, 1, 5));
+        getContentPane().setLayout(new BorderLayout());
+
+        employeePanel.setLayout(new BorderLayout());
+        getContentPane().add(employeePanel, BorderLayout.CENTER);
+
+        ArrayList<MenuData> data = MenuData.getDataAdmin();
+        Menu.getInstance(data).setPreferredSize(new Dimension(150,150));
+        add(Menu.getInstance(data), BorderLayout.WEST);
+
+        topPanel = new JPanel();
+        topPanel.setPreferredSize(new Dimension(1000, 120));
+        topPanel.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(255, 255, 255)));
+        topPanel.setBackground(new Color(0, 38, 77));
+        employeePanel.add(topPanel, BorderLayout.NORTH);
+        topPanel.setLayout(new GridLayout(0, 1, 0, 0));
+
+        lbl_title = new JLabel("      Home/Role Management");
+        lbl_title.setForeground(new Color(255, 255, 255));
+        lbl_title.setFont(new Font("Arial Black", Font.BOLD, 17));
+        lbl_title.setPreferredSize(new Dimension(20,30));
+        topPanel.add(lbl_title);
+
+        titleLabel = new JLabel("   Role Management");
+        titleLabel.setForeground(new Color(255, 255, 255));
+        titleLabel.setFont(new Font("Arial Black", Font.PLAIN, 30));
+        topPanel.add(titleLabel);
+
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(20,20));
+        panel.setBackground(new Color(0, 38, 77));
+        topPanel.add(panel);
+
+        centerPanel = new JPanel();
+        centerPanel.setPreferredSize(new Dimension(60, 50));
+        centerPanel.setBackground(new Color(0, 38, 77));
+        employeePanel.add(centerPanel, BorderLayout.CENTER);
+        GridBagLayout gbl_centerPanel = new GridBagLayout();
+        gbl_centerPanel.columnWidths = new int[]{86, 224, 0, 172, 144, 0, 0};
+        gbl_centerPanel.rowHeights = new int[]{0, 36, 0, 0};
+        gbl_centerPanel.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+        gbl_centerPanel.rowWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+        centerPanel.setLayout(gbl_centerPanel);
+
+        lblNewLabel_1 = new JLabel("Search");
+        lblNewLabel_1.setForeground(new Color(255, 255, 255));
+        lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+        GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+        gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+        gbc_lblNewLabel_1.gridx = 0;
+        gbc_lblNewLabel_1.gridy = 1;
+        centerPanel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+        textField = new JTextField();
+        GridBagConstraints gbc_textField = new GridBagConstraints();
+        gbc_textField.insets = new Insets(0, 0, 5, 5);
+        gbc_textField.fill = GridBagConstraints.BOTH;
+        gbc_textField.gridx = 1;
+        gbc_textField.gridy = 1;
+        centerPanel.add(textField, gbc_textField);
+        textField.setColumns(10);
+
+        btnSearch = new JButton("Search");
+        btnSearch.setPreferredSize(new Dimension(100, 30));
+        GridBagConstraints gbc_btnSearch = new GridBagConstraints();
+        gbc_btnSearch.insets = new Insets(0, 0, 5, 5);
+        gbc_btnSearch.gridx = 2;
+        gbc_btnSearch.gridy = 1;
+        centerPanel.add(btnSearch, gbc_btnSearch);
+
+        bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(0, 38, 77));
+        employeePanel.add(bottomPanel, BorderLayout.SOUTH);
+        bottomPanel.setLayout(new BorderLayout());
+
+        panel_table = new JPanel();
+        panel_table.setForeground(new Color(0, 38, 77));
+        panel_table.setBackground(new Color(0, 38, 77));
+        panel_table.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "List of roles permision", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+        panel_table.setPreferredSize(new Dimension(600,400));
+        panel_table.setLayout(new BorderLayout());
+        bottomPanel.add(panel_table, BorderLayout.CENTER);
+        
+        panel_Role = new JPanel();
+        panel_Role.setBackground(new Color(0, 38, 77));
+        panel_Role.setLayout(new GridLayout(1,0));
+        panel_Role.add(Content.getInstance());
+        panel_table.add(panel_Role, BorderLayout.CENTER);
+
+        panel_Model = new JPanel();
+        panel_Model.setBackground(new Color(0, 38, 77));
+        panel_Model.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Button list", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(255, 255, 255)));
+        panel_Model.setPreferredSize(new Dimension(200,80));
+        panel_table.add(panel_Model, BorderLayout.SOUTH);
+        bottomPanel.add(panel_table, BorderLayout.CENTER);
+
+        btnAdd = new JButton("Add Role");
+        btnAdd.setPreferredSize(new Dimension(200,40));
+        panel_Model.add(btnAdd);
+        btnAdd.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Add addEmployee = new Add();
+                addEmployee.setVisible(true);
+
+            }
+        });
+
+
+
+        btnDelete = new JButton("Delete Role");
+        btnDelete.setPreferredSize(new Dimension(200,40));
+        panel_Model.add(btnDelete);
+        btnDelete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int choice = JOptionPane.showConfirmDialog(null,"Do you want to delete this Role?","Confirm Deletion", JOptionPane.YES_NO_OPTION);
+                if (choice == JOptionPane.YES_OPTION) {
+                } else {}
+            }
+        });
+
+        btnEdit = new JButton("Edit Role");
+        btnEdit.setPreferredSize(new Dimension(200,40));
+        panel_Model.add(btnEdit);
+        btnEdit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	 Edit editEmployee = new Edit();
+                 editEmployee.setVisible(true);
+            }
+        });
+   
+
+
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+            	RoleManagement frame = new RoleManagement();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
