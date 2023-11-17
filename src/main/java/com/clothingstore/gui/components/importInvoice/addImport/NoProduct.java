2866394464a867ee1f2daa809044b6d9d104237b
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2023-11-17 14:47:03 +0700

    logic import item when don't have product

--- src/main/java/com/clothingstore/gui/components/importInvoice/addImport/NoProduct.java
+++ src/main/java/com/clothingstore/gui/components/importInvoice/addImport/NoProduct.java
@@ -1,7 +1,19 @@
 package com.clothingstore.gui.components.importInvoice.addImport;
 
-import javax.swing.*  ;
+import javax.swing.*;
+import java.awt.*;
 
 public class NoProduct extends JPanel {
+
+    public NoProduct() {
+        setLayout(new BorderLayout());
+        JLabel noProductLabel = new JLabel("No products available.");
+        noProductLabel.setHorizontalAlignment(JLabel.CENTER);
+        Font labelFont = new Font("Arial", Font.BOLD, 16);
+        noProductLabel.setFont(labelFont);
+        add(noProductLabel, BorderLayout.CENTER);
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(100, 20));
+    }
 
 }
